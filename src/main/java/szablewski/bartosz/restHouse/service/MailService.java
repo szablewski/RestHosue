d04ae23a99b6156584598af312a0f26738c26ee6
bszablewski <bartosz.szablewski123@wp.pl>
2020-03-25 08:55:36 +0100

    - email sending functionality

---
+++ src/main/java/szablewski/bartosz/restHouse/service/MailService.java
@@ -0,0 +1,29 @@
+package szablewski.bartosz.restHouse.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class MailService {
+
+    private JavaMailSenderImpl javaMailSender;
+
+    @Autowired
+    public MailService(JavaMailSenderImpl javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail(String to, String subject, String text, boolean isHtmlContent) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(text, isHtmlContent);
+        javaMailSender.send(mimeMessage);
+    }
+}
